aab3086fd07099170528c4b404dd972d19f66b7b
vannn-tech <jovan20166@gmail.com>
2026-04-06 17:45:19 +0700

    Input & output array kedua

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -8,6 +8,24 @@ int main() {
     aku[1] = 200;
     cout << endl;
     cout << "Data pada index 1 = " << aku[1] << endl;
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
+
+    cout << "Data pada index 0 = " << kamu[0] << endl;
+    cout << "Data pada index 1 = " << kamu[1] << endl;
+    cout << "Data pada index 2 = " << kamu[2] << endl;
+    cout << "Data pada index 3 = " << kamu[3] << endl;
+    cout << "Data pada index 4 = " << kamu[4] << endl;
+    cout << endl;
 
 
 return 0;
