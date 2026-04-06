49f4c40beca0336954fa8a6e8315404c7c26c018
vannn-tech <jovan20166@gmail.com>
2026-04-06 18:43:24 +0700

    Output data

--- structNew.cpp
+++ structNew.cpp
@@ -18,5 +18,11 @@ int main()
     cin >> mhs.alamat;
     cout << "Umur : ";
     cin >> mhs.umur;
-   
+    cout << endl;
+
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+    
 }
