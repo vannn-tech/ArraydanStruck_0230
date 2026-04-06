9f3454b95a160bd838530a600f39f78f9996fc25
vannn-tech <jovan20166@gmail.com>
2026-04-06 18:45:22 +0700

    Membuat struct dasar

--- structNew2.cpp
+++ structNew2.cpp
@@ -16,21 +16,5 @@ struct orang
 
 int main() {
     orang mhs;
-    // mengisi data
-    cout << "Nama : ";
-    getline(cin, mhs.nama);
-    cout << "Desa : ";
-    getline(cin, mhs.alamat.desa);
-    cout << "Kota : ";
-    getline(cin, mhs.alamat.kota);
-    cout << "Umur : ";
-    cin >> mhs.umur;
-    cout << endl;
 
-    // menampilkan data
-    cout << "Menampilkan Data" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
-    cout << "Umur : " << mhs.umur << endl;
- 
 }
