05d173dff6b268d1fcf5140f6927796a10efb717
vannn-tech <jovan20166@gmail.com>
2026-04-06 17:44:45 +0700

    Update nilai array

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -5,6 +5,10 @@ int main() {
     int aku[5] = {10, 20, 30, 40, 50};
     int kamu[5];
     cout << "Data pada index 1 = " << aku[1] << endl;
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
+
 
 return 0;
 }
