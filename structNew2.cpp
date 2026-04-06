c935db82c1a36ced769005959853deaee0c15611
vannn-tech <jovan20166@gmail.com>
2026-04-06 18:51:15 +0700

    Input data

--- structNew2.cpp
+++ structNew2.cpp
@@ -16,5 +16,15 @@ struct orang
 
 int main() {
     orang mhs;
-
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    
 }
